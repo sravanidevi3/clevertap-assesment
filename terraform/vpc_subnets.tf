db571a3549be48547f67d0d58e6b203c5eddc524
balarcm2010 <62068828+balarcm2010@users.noreply.github.com>
2021-08-18 23:57:19 +0530

    Update vpc_subnets.tf

--- terraform/vpc_subnets.tf
+++ terraform/vpc_subnets.tf
@@ -1,15 +1,6 @@
-resource "aws_vpc" "vpc-clevertap" {
-    cidr_block = "10.0.0.0/16"
-    enable_dns_support = "true"
-    enable_dns_hostnames = "true"
-       tags = {
-         "name" = "vpc-clevertap"
-       }
-}
-
 resource "aws_subnet" "subnet-clevertap-1" {
     vpc_id = "${aws_vpc.vpc-clevertap.id}"
-    cidr_block = "10.0.0.0/16"
+    cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
     availability_zone = "us-east-1a"
      tags = {
@@ -19,7 +10,7 @@ resource "aws_subnet" "subnet-clevertap-1" {
 
 resource "aws_subnet" "subnet-clevertap-2" {
     vpc_id = "${aws_vpc.vpc-clevertap.id}"
-    cidr_block = "10.0.0.0/16"
+    cidr_block = "10.0.0.0/24"
     map_public_ip_on_launch = "true"
     availability_zone = "us-east-1b"
      tags = {
