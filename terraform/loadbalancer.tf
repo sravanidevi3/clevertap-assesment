da61a123fb69dfc7d9183f21c5cf50e5bbecbe22
balarcm2010 <62068828+balarcm2010@users.noreply.github.com>
2021-08-19 12:25:18 +0530

    Update loadbalancer.tf

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -8,21 +8,22 @@ resource "aws_lb" "lb-wordpress-clevertap" {
 
 resource "aws_lb_target_group" "targetgrp-lb-clevertap" {
     name = "targetgrp-lb-clevertap"
-    port = var.health_check.port
-    protocol = var.health_check.protocol
+    port = 80
+    protocol = "HTTP"
     vpc_id = aws_vpc.vpc-clevertap.id
+    proxy_protocol_v2 = "false"
     target_type = "ip"
 
     health_check {
       enabled             = "true"
-       port                = var.health_check.port
-       interval            = var.health_check.interval
-       timeout             = var.health_check.timeout
-       path                = var.health_check.path
-       protocol            = var.health_check.protocol
-       unhealthy_threshold = var.health_check.unhealthy_threshold    
-       matcher             = var.health_check.matcher
-       healthy_threshold   = var.health_check.healthy_threshold
+       port                = 80
+       interval            = 20
+       timeout             = 15
+       path                = "/wp-admin/setup-config.php"
+       protocol            = "HTTP"
+       unhealthy_threshold = 15
+       matcher             = 200
+       healthy_threshold   = 15
   }
 }
 
@@ -39,8 +40,6 @@ resource "aws_lb_listener" "lb-wordpress-clevertap" {
 
 resource "aws_lb_listener_rule" "rule-lb-clevertap" {
     listener_arn = aws_lb_listener.lb-wordpress-clevertap.arn
-    priority = 100
-
     action {
       type = "forward"
       target_group_arn = aws_lb_target_group.targetgrp-lb-clevertap.arn
