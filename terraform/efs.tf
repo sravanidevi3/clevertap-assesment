b288050c36ae35f27fbbc2406ee51bcc908130c3
balarcm2010 <62068828+balarcm2010@users.noreply.github.com>
2021-08-19 14:43:42 +0530

    Update efs.tf

--- terraform/efs.tf
+++ terraform/efs.tf
@@ -5,6 +5,6 @@ resource "aws_efs_file_system" "wordpress" {
 }
 resource "aws_efs_mount_target" "wordpress-mount" {
   file_system_id = aws_efs_file_system.wordpress.id
-  subnet_id      = aws_subnet.subnet-clevertap-1.id
+  subnet_id      = data.aws_subnet_ids.subnets.ids
   
 }
