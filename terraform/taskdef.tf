15ee1471f3fe27daa1c8abad08c2f33b646fbf5f
balarcm2010 <62068828+balarcm2010@users.noreply.github.com>
2021-08-19 12:45:11 +0530

    Update taskdef.tf

--- terraform/taskdef.tf
+++ terraform/taskdef.tf
@@ -4,7 +4,7 @@ resource "aws_ecs_task_definition" "taskdef-clevertap-wordpress" {
   requires_compatibilities = ["FARGATE"]
   memory                   = "4096"
   cpu                      = "2048"
-  execution_role_arn       = "arn:aws:iam::900024488048:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
+  execution_role_arn       = "arn:aws:iam::900024488048:role/ecsecrTaskExecutionRole"
   container_definitions    = jsonencode([
     {
       name = "taskdef-clevertap-wordpress"
