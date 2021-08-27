7565413964873af353e00aaa08541f832785e730
balarcm2010 <62068828+balarcm2010@users.noreply.github.com>
2021-08-27 21:36:09 +0530

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -9,6 +9,7 @@ resource "aws_db_instance" "clevertap" {
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
   vpc_security_group_ids   = ["${aws_security_group.default.id}"]
+  db_subnet_group_name = aws_db_subnet_group.clevertap.name
 }
 
 resource "aws_db_subnet_group" "clevertap" {
