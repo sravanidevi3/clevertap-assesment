f2be8f299ba9fd866d2a4b78082e8cae712aad54
balarcm2010 <62068828+balarcm2010@users.noreply.github.com>
2021-08-27 22:14:28 +0530

    Update secrets.tf

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,3 +1,9 @@
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "_%@"
+}
+
 resource "aws_secretsmanager_secret" "clevertap" {
    name = "Masteraccoundb"
 }
@@ -7,7 +13,7 @@ resource "aws_secretsmanager_secret_version" "clevertapversion" {
   secret_string = <<EOF
    {
     "username": "admin",
-    "password": "admin123"
+    "password": "${random_password.password.result}"
    }
 EOF
 }
