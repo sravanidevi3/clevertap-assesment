ac2450920ac02024ceeaeeb0a5a3058016e6d746
balarcm2010 <62068828+balarcm2010@users.noreply.github.com>
2021-08-28 11:22:02 +0530

    Update secrets.tf

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,15 +1,18 @@
-resource "random_password" "password" { 
-  length = 16 
-  special = true 
+resource "random_password" "password" {
+  length           = 16
+  special          = true
   override_special = "_%@"
 }
-
-resource "aws_secretsmanager_secret" "clevertap" { 
-  name = "wordpresssecrets"
+resource "aws_secretsmanager_secret" "clevertap" {
+   name = "wordpresssecrets"
 }
-resource "aws_secretsmanager_secret_version" "clevertapversion" { 
-  secret_id = aws_secretsmanager_secret.clevertap.id 
-  secret_string = <<EOF { "username": "admin", "password": "${random_password.password.result}" 
-  }
-  EOF
+
+resource "aws_secretsmanager_secret_version" "clevertapversion" {
+  secret_id = aws_secretsmanager_secret.clevertap.id
+  secret_string = <<EOF
+   {
+    "username": "admin",
+    "password": "${random_password.password.result}"
+   }
+EOF
 }
