f0d6afc60afac620ee4a314e317588b3f261702b
balarcm2010 <62068828+balarcm2010@users.noreply.github.com>
2021-08-28 11:12:43 +0530

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,3 +1,17 @@
+data "aws_secretsmanager_secret" "clevertap" {
+  arn = aws_secretsmanager_secret.clevertap.arn
+}
+
+data "aws_secretsmanager_secret_version" "clevertapversion" {
+  secret_id = data.aws_secretsmanager_secret.clevertap.arn
+}
+
+locals {
+  db_creds = jsondecode(
+  data.aws_secretsmanager_secret_version.clevertapversion.secret_string
+   )
+}
+
 resource "aws_db_instance" "clevertap" {
   allocated_storage    = 10
   engine               = "mysql"
@@ -5,8 +19,8 @@ resource "aws_db_instance" "clevertap" {
   instance_class       = "db.t2.micro"
   name                 = "clevertap"
   identifier           = "clevertap"
-  username             = "admin"
-  password             = "admin123"
+  username             = local.db_creds.username
+  password             = local.db_creds.password
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
   vpc_security_group_ids   = ["${aws_security_group.default.id}"]
